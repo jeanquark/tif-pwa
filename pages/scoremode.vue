797515b5513235cb39e5ff5d78093cf3c0e191ab
Macgama <33863329+macgama@users.noreply.github.com>
2019-01-08 16:23:06 +0100

    Add files via upload

--- pages/scoremode.vue
+++ pages/scoremode.vue
@@ -126,7 +126,7 @@
 																<div style="background-color: red; height: 100%; width: 2px"></div>
 															</v-flex>
 															<v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
-																<div style="color: orange;font-size: 80%"><span style="float: left; background-color: red; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px"> {{ props.item.status }}</span> {{ props.item.date }} - {{ props.item.time}}</div>
+																<div style="color: orange;font-size: 80%"><span style="float: left; background-color: red; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" v-if="props.item.status === 'IN PLAY' || props.item.status === 'HALF TIME BREAK' || props.item.status === 'ADDED TIME' || props.item.status === 'FINISHED'"></span> {{ props.item.date }} - {{ props.item.time}}</div>
 															</v-flex>
 														</v-layout>
 														<v-layout align-center style="max-width: 100%">
