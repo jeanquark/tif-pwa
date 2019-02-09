1258b78f710271667d0488da27450202a2b3b6ec
macgama <gael.manigley@gmail.com>
2019-02-09 12:55:04 +0100

    Update scoremode.vue

--- pages/scoremode.vue
+++ pages/scoremode.vue
@@ -694,176 +694,331 @@
     }
         
     /* End Content */
-    	
-		.menuSport {
-			height: 40px;
-		}
-		
-		.menuDay {
-			height: 40px;
-		}
-		
-		.v-expansion-panel__header {
-		padding: 2px 2px;
-		min-height: 24px;
-		}
+
+	.menuSport {
+		height: 40px;
+	}
 	
-		.fade-enter-active, .fade-leave-active {
-			transition: opacity 5s;
-			background-color: #000;
+	.menuDay {
+		height: 40px;
+	}
+	
+	.v-expansion-panel__header {
+	padding: 2px 2px;
+	min-height: 24px;
+	}
+
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity 5s;
+		background-color: #000;
+	}
+	
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
+	
+	.imgLogoEquipe {
+	max-width: 30px;
+	max-height: 30px;
+	}
+	
+	.teamTextSize {
+	font-size: 1.3em;
+	}
+
+	.black {
+	background-color: black;
+	}
+	
+	.imageLogo {
+	width: 30px;
+	height: 30px;
+	}
+	
+	/* ScoreCard */
+
+	.card {
+	border-radius: 5px;
+	}
+	.card-title {
+	background-color: lightslategray;
+	}
+	.card-text {
+	background-color: whitesmoke;
+	width: auto;
+	border-radius: 8px;
+	padding: 8px;
+	}
+	.card-footer {
+	background-color: lightslategrey;
+	}
+	.fas:hover {
+	cursor: pointer;
+	color: #fff;
+	}
+	
+	.userText {
+	font-size: 1.3em;
+	}
+	
+	.connectText {
+	font-size: 1.3em;
+	}
+	
+	.chiffres {
+	font-size: 2.5em;
+	color: orange;
+	font-width: 700;
+	}
+	
+	.activity {
+	font-size: 1.5em;
+	text-transform: uppercase;
+	}
+	
+	.levelBox {
+	background-color: #757575;
+	color: black;
+	vertical-align: middle;
+	padding: 2px;
+	border-radius: 3px;
+	width: 95%;
+	margin: auto;
+	}
+	
+	.energyBox {
+	background-color: #757575;
+	color: black;
+	vertical-align: middle;
+	padding: 2px;
+	border-radius: 3px;
+	width: 95%;
+	margin: auto;
+	}
+	
+	.dollarBox {
+	background-color: #757575;
+	color: black;
+	vertical-align: middle;
+	padding: 2px;
+	border-radius: 3px;
+	width: 95%;
+	margin: auto;
+	}
+
+	.tokenBox {
+	background-color: #757575;
+	color: black;
+	vertical-align: middle;
+	padding: 2px;
+	border-radius: 3px;
+	width: 95%;
+	margin: auto;
+	}
+
+	/* Menu */
+
+	#dock-container {
+	height: 80px;
+	padding: 0;
+	margin: 0;
+	bottom: 0;
+	background-color: rgb(248,147,37);
+	border: none;
+	border-top: 4px solid orangered;
+	}
+
+	#dock-container li#active img {
+	-webkit-transform: scale(1.65);
+	margin: 0 0.5em;
+	}
+
+	#dock-container li {
+	width: 17%;
+	padding: 0;
+	margin: 0;
+	list-style-type: none;
+	display: inline-block;
+	position: relative;
+	padding-top: 10px;
+	}
+
+	#dock-container ul {
+	width: 100%;
+	padding-left: 0px;
+	padding-right: 0px;
+	margin-bottom: 0;
+	}
+
+	#dock-container li img {
+	width: 58px;
+	height: 58px;
+	-webkit-gradient: (linear, left top, left bottom, from(transparent), color-stop(0.7, transparent), to(rgba(255,255,255,.5)));
+	-webkit-transition: all 0.3s;
+	-webkit-transform-origin: 50% 100%;
+	}
+
+	#dock-container li:hover img { 
+	-webkit-transform: scale(1.65);
+	margin: 0 0.5em;
+	}
+
+	#dock-container li:hover + li img, #dock-container li.prev img {
+	-webkit-transform: scale(1);
+	margin: 0 0;
+	}
+
+	#dock-container li span {
+	display: none;
+	position: absolute;
+	bottom: 0px;
+	left: 0;
+	width: 100%;
+	background-color: rgba(0,0,0,0.5);
+	border-radius: 5px;
+	}
+
+	#dock-container li#active span {
+	display: none;
+	position: absolute;
+	bottom: 0px;
+	left: 0;
+	width: 100%;
+	background-color: rgba(0,0,0,0.5);
+	border-radius: 5px;
+	}
+
+	#dock-container li#active span {
+	display: block;
+	color: #fff;
+	}
+
+	#dock-container li#active .textMenu {
+	font: normal 120%/1 "Acme", Helvetica, sans-serif;
+	padding: 2px;
+	}
+
+	#dock-container li:hover span {
+	display: block;
+	color: #fff;
+	}
+
+	#dock-container .textMenu {
+	font: normal 120%/1 "Acme", Helvetica, sans-serif;
+	padding: 2px;
+	}
+
+		@media only screen and (max-width: 768px) {
+
+		#app {
+		font: normal 90%/1 "Acme", Helvetica, sans-serif;
 		}
 		
-		.fade-enter, .fade-leave-to {
-			opacity: 0;
+		.pd-right10 {
+		padding-right: 10px;
+		}
+		
+		.pd-left10 {
+		padding-left: 10px;
 		}
 		
 		.imgLogoEquipe {
-		max-width: 30px;
-		max-height: 30px;
+		max-width: 35px;
 		}
 		
 		.teamTextSize {
-		font-size: 1.3em;
-		}
-	
-		.black {
-		background-color: black;
-		}
-		
-		.imageLogo {
-		width: 30px;
-		height: 30px;
-		}
-		
-		/* ScoreCard */
-
-		.card {
-		border-radius: 5px;
-		}
-		.card-title {
-		background-color: lightslategray;
-		}
-		.card-text {
-		background-color: whitesmoke;
-		width: auto;
-		border-radius: 8px;
-		padding: 8px;
-		}
-		.card-footer {
-		background-color: lightslategrey;
-		}
-		.fas:hover {
-		cursor: pointer;
-		color: #fff;
-		}
-			
-		#app {
-		font: normal 100%/1 "Acme", Helvetica, sans-serif;
-		}
-		
-		.userText {
-		font-size: 1.3em;
-		}
-		
-		.connectText {
-		font-size: 1.3em;
-		}
-
-		/* Header */
-		
-		.headerMenu {
-		height: 91px;
-		}
-		
-		.headerInfo {
-		padding: 0px; margin-top: 0px;
-		}
-		
-		.activeLeft {
-		padding: 0px; height: 20px; margin-top: 0px;
-		}
-		
-		.activeRight {
-		padding: 0px; height: 20px; margin-top: 0px;
-		}
-		
-		.backBlack {
-		background-image: url("/images/header-TIF_03.png");
-		background-position: center;
-		background-repeat: repeat-x;
-		}
-		
-		.headerLogo {
-		height: auto;
+		font-size: 1.0em;
 		}
 		
 		.headerMenus {
 		padding: 0; 
 		margin: 0; 
-		height: 111px;
-		}
-		
-		.chiffres {
-		font-size: 2.5em;
-		color: orange;
-		font-width: 700;
-		}
-		
-		.activity {
-		font-size: 1.5em;
-		text-transform: uppercase;
-		}
-		
-		.levelBox {
-		background-color: #757575;
-		color: black;
-		vertical-align: middle;
-		padding: 2px;
-		border-radius: 3px;
-		width: 95%;
-		margin: auto;
+		height: 46px;
 		}
 		
 		.energyBox {
 		background-color: #757575;
 		color: black;
 		vertical-align: middle;
-		padding: 2px;
-		border-radius: 3px;
+		padding: 1px;
+		border-radius: 2px;
 		width: 95%;
 		margin: auto;
+		font-size: 0.8em;
 		}
 		
-		.dollarBox {
-		background-color: #757575;
-		color: black;
-		vertical-align: middle;
-		padding: 2px;
-		border-radius: 3px;
-		width: 95%;
-		margin: auto;
+		.userText {
+		font-size: 0.8em;
+		}
+		
+		.connectText {
+		font-size: 0.8em;
 		}
 
-		.tokenBox {
-		background-color: #757575;
-		color: black;
-		vertical-align: middle;
-		padding: 2px;
-		border-radius: 3px;
-		width: 95%;
-		margin: auto;
+		/* Header */
+
+		.imageLogoTif {
+		width: 60px;
+		height: 60px;
+		}
+		
+		.backBlack {
+		background-image: url("/images/header-TIF_03.png");
+		background-position: center;
+		background-repeat: repeat-x;
+		background-size: contain;
+		height: 60px;
 		}
 
+		.boxTif {
+		width: 60px;
+		height: 60px;
+		}
+
+		.imageLogo {
+		width: 25px;
+		height: 25px;
+		}
+		
+		.menuSport {
+			height: 30px;
+		}
+		
+		.menuDay {
+			height: 30px;
+		}
+		
+		.headerLogo {
+		height: auto;
+		}
+		
+		.headerMenu {
+		height: 46px;
+		}
+		
+		.v-expansion-panel__header {
+		padding: 2px 2px;
+		min-height: 24px;
+		}
+		
+		.chiffres {
+		font-size: 1.2em;
+		}
+		
+		.activity {
+		font-size: 0.8em;
+		}
+		
 		/* Menu */
 
 		#dock-container {
-		height: 80px;
+		height: 55px;
 		padding: 0;
 		margin: 0;
 		bottom: 0;
 		background-color: rgb(248,147,37);
 		border: none;
-		border-top: 4px solid orangered;
+		border-top: 4px solid darkred;
 		}
 
 		#dock-container li#active img {
@@ -878,7 +1033,7 @@
 		list-style-type: none;
 		display: inline-block;
 		position: relative;
-		padding-top: 10px;
+		padding-top: 3px;
 		}
 
 		#dock-container ul {
@@ -889,8 +1044,8 @@
 		}
 
 		#dock-container li img {
-		width: 58px;
-		height: 58px;
+		width: 38px;
+		height: 38px;
 		-webkit-gradient: (linear, left top, left bottom, from(transparent), color-stop(0.7, transparent), to(rgba(255,255,255,.5)));
 		-webkit-transition: all 0.3s;
 		-webkit-transform-origin: 50% 100%;
@@ -932,7 +1087,7 @@
 		}
 
 		#dock-container li#active .textMenu {
-		font: normal 120%/1 "Acme", Helvetica, sans-serif;
+		font: normal 90%/1 "Acme", Helvetica, sans-serif;
 		padding: 2px;
 		}
 
@@ -942,202 +1097,9 @@
 		}
 
 		#dock-container .textMenu {
-		font: normal 120%/1 "Acme", Helvetica, sans-serif;
+		font: normal 90%/1 "Acme", Helvetica, sans-serif;
 		padding: 2px;
 		}
 
-			@media only screen and (max-width: 768px) {
-
-			#app {
-			font: normal 90%/1 "Acme", Helvetica, sans-serif;
-			}
-			
-			.pd-right10 {
-			padding-right: 10px;
-			}
-			
-			.pd-left10 {
-			padding-left: 10px;
-			}
-			
-			.imgLogoEquipe {
-			max-width: 35px;
-			}
-			
-			.teamTextSize {
-			font-size: 1.0em;
-			}
-			
-			.headerMenus {
-			padding: 0; 
-			margin: 0; 
-			height: 46px;
-			}
-			
-			.energyBox {
-			background-color: #757575;
-			color: black;
-			vertical-align: middle;
-			padding: 1px;
-			border-radius: 2px;
-			width: 95%;
-			margin: auto;
-			font-size: 0.8em;
-			}
-			
-			.userText {
-			font-size: 0.8em;
-			}
-			
-			.connectText {
-			font-size: 0.8em;
-			}
-
-			/* Header */
-
-			.imageLogoTif {
-			width: 60px;
-			height: 60px;
-			}
-			
-			.backBlack {
-			background-image: url("/images/header-TIF_03.png");
-			background-position: center;
-			background-repeat: repeat-x;
-			background-size: contain;
-			height: 60px;
-			}
-
-			.boxTif {
-			width: 60px;
-			height: 60px;
-			}
-
-			.imageLogo {
-			width: 25px;
-			height: 25px;
-			}
-			
-			.menuSport {
-				height: 30px;
-			}
-			
-			.menuDay {
-				height: 30px;
-			}
-			
-			.headerLogo {
-			height: auto;
-			}
-			
-			.headerMenu {
-			height: 46px;
-			}
-			
-			.v-expansion-panel__header {
-			padding: 2px 2px;
-			min-height: 24px;
-			}
-			
-			.chiffres {
-			font-size: 1.2em;
-			}
-			
-			.activity {
-			font-size: 0.8em;
-			}
-			
-			/* Menu */
-
-			#dock-container {
-			height: 55px;
-			padding: 0;
-			margin: 0;
-			bottom: 0;
-			background-color: rgb(248,147,37);
-			border: none;
-			border-top: 4px solid darkred;
-			}
-
-			#dock-container li#active img {
-			-webkit-transform: scale(1.65);
-			margin: 0 0.5em;
-			}
-
-			#dock-container li {
-			width: 17%;
-			padding: 0;
-			margin: 0;
-			list-style-type: none;
-			display: inline-block;
-			position: relative;
-			padding-top: 3px;
-			}
-
-			#dock-container ul {
-			width: 100%;
-			padding-left: 0px;
-			padding-right: 0px;
-			margin-bottom: 0;
-			}
-
-			#dock-container li img {
-			width: 38px;
-			height: 38px;
-			-webkit-gradient: (linear, left top, left bottom, from(transparent), color-stop(0.7, transparent), to(rgba(255,255,255,.5)));
-			-webkit-transition: all 0.3s;
-			-webkit-transform-origin: 50% 100%;
-			}
-
-			#dock-container li:hover img { 
-			-webkit-transform: scale(1.65);
-			margin: 0 0.5em;
-			}
-
-			#dock-container li:hover + li img, #dock-container li.prev img {
-			-webkit-transform: scale(1);
-			margin: 0 0;
-			}
-
-			#dock-container li span {
-			display: none;
-			position: absolute;
-			bottom: 0px;
-			left: 0;
-			width: 100%;
-			background-color: rgba(0,0,0,0.5);
-			border-radius: 5px;
-			}
-
-			#dock-container li#active span {
-			display: none;
-			position: absolute;
-			bottom: 0px;
-			left: 0;
-			width: 100%;
-			background-color: rgba(0,0,0,0.5);
-			border-radius: 5px;
-			}
-
-			#dock-container li#active span {
-			display: block;
-			color: #fff;
-			}
-
-			#dock-container li#active .textMenu {
-			font: normal 90%/1 "Acme", Helvetica, sans-serif;
-			padding: 2px;
-			}
-
-			#dock-container li:hover span {
-			display: block;
-			color: #fff;
-			}
-
-			#dock-container .textMenu {
-			font: normal 90%/1 "Acme", Helvetica, sans-serif;
-			padding: 2px;
-			}
-
-		}
-	</style>
+	}
+</style>
