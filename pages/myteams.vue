ad01c6acce5c4b90f58ce3b2238ef7371e996b9d
Macgama <33863329+macgama@users.noreply.github.com>
2019-02-13 16:57:44 +0100

    Add files via upload

--- pages/myteams.vue
+++ pages/myteams.vue
@@ -4,7 +4,7 @@
 			<v-card class="card">
 				<v-card-title primary-title>
 					<v-flex xs6>
-						<h2 class="text-xs-left" style="color: white; font-family: Acme">Fan de...</h2>
+						<h1 class="text-xs-left" style="color: white; font-family: Acme">Fan de...</h1>
 					</v-flex>
 					<v-flex xs6 class="text-xs-right">
 						<fa :icon="['fas', 'arrow-circle-left']" size="2x" class="icon" @click="goBack" />
@@ -12,8 +12,8 @@
 				</v-card-title>
 
 				<v-card-text class="card-text mx-3">
-					<div class="text-xs-center" style="color: white; font-family: Acme">
-						<h3>Tu gères des fans de {{ loadedUserTeams.length > 1 ? loadedUserTeams.length + ' équipes' : loadedUserTeams.length + ' équipe' }} :</h3>
+					<div class="text-xs-center" style="color: black; font-family: Acme">
+						<h2>Tu gères des fans de {{ loadedUserTeams.length > 1 ? loadedUserTeams.length + ' équipes' : loadedUserTeams.length + ' équipe' }} :</h2>
 						<!-- <b>loadedUserTeams:</b> {{ loadedUserTeams }}<br /><br /> -->
 						<!-- <b>loadedCompetitions:</b> {{ loadedCompetitions }}<br /><br /> -->
 						<!-- <b>loadedTeamsByCompetition:</b> {{ loadedTeamsByCompetition }}<br /><br /> -->
