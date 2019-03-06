9768e73cf2d3d2aa9f846bf52dfbc2f830622abe
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-06 07:00:20 +0100

    Add files via upload

--- pages/gamemode_jm.vue
+++ pages/gamemode_jm.vue
@@ -105,42 +105,42 @@
             
             <!-- Scrollable content -->
             <div class="content">
-				<div class="cadreScore" style="position: absolute; top: 20px; left: 100px">
-                    <v-flex xs12 style="margin: 0; padding-top: 2px; padding-bottom: 2px; height: 100%">
-                        <v-layout align-start>
-                            <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 15px; margin: 0">
-                                <div style="background-color: red; height: 100%; width: 2px"></div>
-                            </v-flex>
-                            <v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
-                                <div style="color: orange;font-size: 80%">
-                                    <span style="float: left; background-color: red; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >BIENTÔT - 19:00 (heure local)</span>
-                                </div>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout align-center style="max-width: 100%">
-                            <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 40px; margin: 0">
-                                <div style="background-color: red; height: 40px; width: 2px"></div>
-                            </v-flex>
-                            <v-flex sm1 hidden-xs-only align-center class="text-xs-center" style="width: 50px; padding-left: 15px">
-                                <img src="/images/switzerland.png" class="imgLogoEquipe"/>
-                            </v-flex>
-                            <v-flex sm4 xs5 align-center class="text-xs-left pd-left10">
-                                <span class="teamTextSize">Liverpool</span>
-                            </v-flex>
-                            <v-flex sm2 xs2 class="text-xs-center">
-                                <span style="background-color: black; color: orange; padding: 2px 10px; border-radius: 5px; font-size: 130%">3 - 2</span>
-                            </v-flex>
-                            <v-flex sm4 xs5 align-center class="text-xs-right pd-right10">
-                                <span class="teamTextSize">Arsenal</span>
-                            </v-flex>
-                            <v-flex sm1 hidden-xs-only align-center class="text-xs-center" style="width: 50px; padding-right: 15px">
-                                <img src="/images/switzerland.png" class="imgLogoEquipe"/>
-                            </v-flex>
-                        </v-layout>
-                    </v-flex>
-                </div>                
                 <!-- <v-layout> -->
                 <!-- <div class="container"> -->
+					<div class="cadreScore" style="position: absolute; top: 20px; left: 100px">
+						<v-flex xs12 style="margin: 0; padding-top: 2px; padding-bottom: 2px; height: 100%">
+							<v-layout align-start>
+								<v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 15px; margin: 0">
+									<div style="background-color: red; height: 100%; width: 2px"></div>
+								</v-flex>
+								<v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
+									<div style="color: orange;font-size: 80%">
+										<span style="float: left; background-color: red; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >BIENTÔT - 19:00 (heure local)</span>
+									</div>
+								</v-flex>
+							</v-layout>
+							<v-layout align-center style="max-width: 100%">
+								<v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 40px; margin: 0">
+									<div style="background-color: red; height: 40px; width: 2px"></div>
+								</v-flex>
+								<v-flex sm1 hidden-xs-only align-center class="text-xs-center" style="width: 50px; padding-left: 15px">
+									<img src="/images/switzerland.png" class="imgLogoEquipe"/>
+								</v-flex>
+								<v-flex sm4 xs5 align-center class="text-xs-left pd-left10">
+									<span class="teamTextSize">Liverpool</span>
+								</v-flex>
+								<v-flex sm2 xs2 class="text-xs-center">
+									<span style="background-color: black; color: orange; padding: 2px 10px; border-radius: 5px; font-size: 130%">3 - 2</span>
+								</v-flex>
+								<v-flex sm4 xs5 align-center class="text-xs-right pd-right10">
+									<span class="teamTextSize">Arsenal</span>
+								</v-flex>
+								<v-flex sm1 hidden-xs-only align-center class="text-xs-center" style="width: 50px; padding-right: 15px">
+									<img src="/images/switzerland.png" class="imgLogoEquipe"/>
+								</v-flex>
+							</v-layout>
+						</v-flex>
+					</div>                
                     <img src="/images/barcelona_clickable.png" usemap="#map" />
                     <!-- <img src="/images/barcelona_clickable.png" usemap="#map" /> -->
                     <map name="map">
@@ -451,7 +451,16 @@
         border-radius: 5px;
         padding: 5px;
     }
-    
+    .cadreScore {
+    border: 2px solid orangered;
+    padding: 5px;
+    width: 800px;
+    background-color: whitesmoke;
+    opacity: 0.9;
+    color: black;
+    text-align: center;
+    font-size: 1.1em;
+    }
     .material-icons.md-18 { font-size: 18px; }
     .material-icons.md-24 { font-size: 24px; }
     .material-icons.md-36 { font-size: 36px; }
