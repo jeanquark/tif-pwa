24c3b230d9af9eb15ee467801efdb74a8322309a
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-13 16:33:58 +0100

    Add files via upload

--- pages/gamemode_jm.vue
+++ pages/gamemode_jm.vue
@@ -111,8 +111,9 @@
 						<v-flex xs12 style="margin: 0; height: 100%">
 							<v-layout align-start>
 								<div class="nameTeam" style="position: relative; font-size: 50px; color: orangered; text-decoration: none; text-transform: uppercase; top: 0px; left: 30px; text-shadow: black 3px 3px 2px;">
-									Espagnol Barcelone <span style="position: relative; top: 3px; font-size: 15px; color: orangered; text-shadow: none; margin-left: 10px;">VS</span> <span style="position: relative; top: 3px; font-size: 25px; color: orangered; text-shadow: none; margin-left: 8px;">Real Madrid</span> <span style="position: relative; top: 3px; font-size: 15px; color: orangered; text-shadow: none; margin-left: 10px;">Mardi, 12 mars 2019 à 19:00</span>
+									Espagnol Barcelone <span style="position: relative; top: 3px; font-size: 15px; color: orangered; text-shadow: none; margin-left: 10px;">VS</span> <span style="position: relative; top: 3px; font-size: 25px; color: orangered; text-shadow: none; margin-left: 8px;">Real Madrid</span>
 								</div>
+								<span style="position: relative; top: 3px; font-size: 15px; color: orangered; text-shadow: none; margin-left: 10px;">Mardi, 12 mars 2019 à 19:00</span>
 							</v-layout>
 							<!-- 
 							<v-layout align-start>
