5d048e164de251e1721087e70222b8df902f9de5
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-15 09:19:58 +0100

    Add files via upload

--- pages/myskill.vue
+++ pages/myskill.vue
@@ -106,57 +106,49 @@
             <!-- Scrollable content -->
             <div class="content">
 				<v-container fluid>
-				  <v-layout row>
-					<v-flex xs6 order-lg2>
-					  <v-card dark tile flat color="error">
-						<v-card-text>#1</v-card-text>
-					  </v-card>
+					<v-layout row>
+						<v-flex xs4 sm4 md4>
+							Menu1
+						</v-flex>
+						<v-flex xs4 sm4 md4>
+							Menu1
+						</v-flex>
+						<v-flex xs4 sm4 md4>
+							Menu1
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="red darken-2">
+					<v-card-text>#1</v-card-text>
+					</v-card>
 					</v-flex>
-					<v-flex xs6>
-					  <v-card dark tile flat color="red darken-4">
-						<v-card-text>#2</v-card-text>
-					  </v-card>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="deep-orange lighten-1">
+					<v-card-text>#2</v-card-text>
+					</v-card>
 					</v-flex>
-				  </v-layout>
-				  <v-layout row>
-					<v-flex xs4 order-md2 order-xs1>
-					  <v-card dark tile flat color="red lighten-1">
-						<v-card-text>#1</v-card-text>
-					  </v-card>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="deep-orange darken-3">
+					<v-card-text>#3</v-card-text>
+					</v-card>
 					</v-flex>
-					<v-flex xs4 order-md3 order-xs2>
-					  <v-card dark tile flat color="red lighten-2">
-						<v-card-text>#2</v-card-text>
-					  </v-card>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="deep-orange">
+					<v-card-text>#4</v-card-text>
+					</v-card>
 					</v-flex>
-					<v-flex xs4 order-md1 order-xs3>
-					  <v-card dark tile flat color="red darken-1">
-						<v-card-text>#3</v-card-text>
-					  </v-card>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="deep-orange">
+					<v-card-text>#6</v-card-text>
+					</v-card>
 					</v-flex>
-				  </v-layout>
-				  <v-layout row wrap>
-					<v-flex xs12 sm6 md3 order-md4 order-sm2>
-					  <v-card dark tile flat color="red darken-2">
-						<v-card-text>#1</v-card-text>
-					  </v-card>
+					<v-flex xs12 sm6 md6>
+					<v-card dark tile flat color="deep-orange">
+					<v-card-text>#6</v-card-text>
+					</v-card>
 					</v-flex>
-					<v-flex xs12 sm6 md3 order-md3 order-sm1>
-					  <v-card dark tile flat color="deep-orange lighten-1">
-						<v-card-text>#2</v-card-text>
-					  </v-card>
-					</v-flex>
-					<v-flex xs12 sm6 md3 order-md2 order-sm4>
-					  <v-card dark tile flat color="deep-orange darken-3">
-						<v-card-text>#3</v-card-text>
-					  </v-card>
-					</v-flex>
-					<v-flex xs12 sm6 md3 order-md1 order-sm3>
-					  <v-card dark tile flat color="deep-orange">
-						<v-card-text>#4</v-card-text>
-					  </v-card>
-					</v-flex>
-				  </v-layout>
+					</v-layout>
 				</v-container>
 			</div>
             
