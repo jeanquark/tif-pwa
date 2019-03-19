fff04cda69b00d88d28dd779d3af864a2d23e1be
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-19 09:08:19 +0100

    Add files via upload

--- components/ScoremodeFooter.vue
+++ components/ScoremodeFooter.vue
@@ -1,36 +1,38 @@
 <template>
-	<v-footer style="background-color: black; position: sticky; bottom: 0; width: 100%" height="auto" class="text-xs-center">
-		<v-container fluid style="padding: 0; max-width: 1000px">
-			<div class="barreOrangered"></div>
-			<div class="barreBlack"></div>
-			<div id="dock-container">
-				<div id="dock">
+		<!-- Fixed Footer -->
+		<div id="footer">
+			<!-- <br /><br /><br />
+			<h2>Footer</h2>
+			<br /><br /><br /> -->
+
+				<div class="barreOrangered"></div>
+				<div class="barreBlack"></div>
+				<div id="dock-container">
 					<ul>
 						<li>
-							<nuxt-link to="/teams"><img src="/images/menuTeam.png">
-							<span class="textMenu">Equipes</span></nuxt-link>
-						</li>
-						<li>
-							<nuxt-link to="/competitions"><img src="/images/menuCompetition.png">
-							<span class="textMenu">Compétitions</span></nuxt-link>
+							<nuxt-link to="/myresult"><img src="/images/menuResultat.png">
+							<span class="textMenu">Mes classements</span></nuxt-link>
 						</li>
 						<li id="active">
-							<nuxt-link to="/scoremode"><img src="/images/menuHome.png">
-							<span class="textMenu">Matches du jour</span></nuxt-link>
+							<nuxt-link to="/myskill"><img src="/images/menuShop.png">
+							<span class="textMenu">Mes compétences</span></nuxt-link>
 						</li>
 						<li>
-							<nuxt-link to="/players"><img src="/images/menuPlayer.png">
-							<span class="textMenu">Joueurs</span></nuxt-link>
+							<nuxt-link to="/gamemode_jm"><img src="/images/menuHome.png">
+							<span class="textMenu">Aujourd'hui</span></nuxt-link>
 						</li>
 						<li>
-							<nuxt-link to="/tifnews"><img src="/images/menuTifnews.png">
-							<span class="textMenu">TIF News</span></nuxt-link>
+							<nuxt-link to="/myinventory"><img src="/images/menuSocial.png">
+							<span class="textMenu">Mon inventaire</span></nuxt-link>
+						</li>
+						<li>
+							<nuxt-link to="/scoremode"><img src="/images/menuResultat.png">
+							<span class="textMenu">ScoreMode</span></nuxt-link>
 						</li>
 					</ul>
 				</div>
-			</div>
-		</v-container>
-	</v-footer>     
+		</div>
+	</v-container>
 </template>
 
 <script>
