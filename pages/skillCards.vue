778172a4d5eee3dcb534c376f2a22fd8999b1c32
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-19 08:05:00 +0100

    Add files via upload

--- pages/skillCards.vue
+++ pages/skillCards.vue
@@ -40,13 +40,13 @@
 												<div>											
 													<v-progress-linear class="progressOrange" color="rgb(255,69,0)" height="20" value="1" style="margin-top: 10px; margin-bottom: 10px"></v-progress-linear><span class="progress-bar-txt-white" style="top: -27px">1%</span>
 													<div style="font-size: 14px; text-align: center; margin-bottom: 10px">Pénalty à la 93e de la finale de la coupe du monde… est-ce que tu oses regarder ?</div>
-													<div style="background-color: green; padding: 8px 5px 5px 5px; text-align: center; margin-top: 10px; border-radius: 5px">
+													<div style="background-color: green; padding: 8px 5px 5px 5px; text-align: center; margin-top: 20px; border-radius: 5px">
 														<b>Actions déjà débloquées :</b></br>
 														Vendeur de voiturette de golf - Niv. 1
 													</div>
 													<div style="background-color: orangered; padding: 8px 5px 5px 5px; text-align: center; margin-top: 10px; border-radius: 5px">
 														<b>À débloquer :</b></br>
-														Démineur - Niv. 2 (Baratin - Niv. 1 + Technologie - Niv. 2)
+														Démineur - Niv. 2 (Baratin - Niv. 2 + Technologie - Niv. 2)
 													</div>
 												</div>
 											</v-card-title>
