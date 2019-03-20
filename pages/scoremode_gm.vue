d91e4983a19f8022c00f1eab7cf9135b5c6dfd3d
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-20 07:21:36 +0100

    Add files via upload

--- pages/scoremode_gm.vue
+++ pages/scoremode_gm.vue
@@ -18,16 +18,20 @@
 						<!-- loadedEventsByDay: {{ loadedEventsByDay('2019-03-02') }}<br /><br /> -->
 
 						<!-- Results -->
-						<v-tabs hidden-xs-only color="rgb(0,128,0)" height="40" slider-color="yellow" fixed-tabs v-model="selectType">
+						<div hidden-xs-only>
+						<v-tabs color="rgb(0,128,0)" height="40" slider-color="yellow" fixed-tabs v-model="selectType">
 							<v-tab v-for="type in types" :key="type.slug" ripple style="cursor: pointer;">
 								<span style="font-size: 1.0em; color: white">{{ type.name }}</span>
 							</v-tab>
 						</v-tabs>
-						<v-tabs hidden-sm-and-up color="rgb(0,128,0)" height="30" slider-color="yellow" fixed-tabs v-model="selectType">
+						</div>
+						<div hidden-sm-and-up>
+						<v-tabs color="rgb(0,128,0)" height="30" slider-color="yellow" fixed-tabs v-model="selectType">
 							<v-tab v-for="type in types" :key="type.slug" ripple style="cursor: pointer;">
 								<span style="font-size: 0.9em; color: white">{{ type.name }}</span>
 							</v-tab>
 						</v-tabs>
+						</div>
 						
 
 						<!-- Results -->
