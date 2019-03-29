1cdd61da0e9ae71e786808129d0c9558838d2715
Macgama <33863329+macgama@users.noreply.github.com>
2019-03-29 14:18:39 +0100

    Add files via upload

--- pages/inventoryAvatar.vue
+++ pages/inventoryAvatar.vue
@@ -122,26 +122,34 @@
 							<v-flex xs12 sm12 md12 style="padding: 5px">
 								<div style="position: relative">
 									<div style="padding: 5px; text-align: center">
-										<div class="hidden-sm-and up" style="position: absolute; top: 5px; left: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
+										<div class="hidden-sm-and-up" style="position: absolute; top: 5px; left: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
 											<span class="inventoryBadgeAvatar fs16">Tête</span>
 										</div>
 										<div class="hidden-xs-only" style="position: absolute; top: 5px; left: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 100px; height: 100px">
 											<span class="inventoryBadgeAvatar fs16">Tête</span>
 										</div>
-										<div style="position: absolute; top: 80px; left: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
+										<div class="hidden-sm-and-up" style="position: absolute; top: 80px; left: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
+											<nuxt-link to="/inventoryCards" style="text-decoration: none">
+												<span class="fs16"><img src="/images/stress.jpg" class="imgInventorySmall"/></span>
+											</nuxt-link>
+										</div>
+										<div class="hidden-xs-only" style="position: absolute; top: 5px; left: 115px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 100px; height: 100px">
 											<nuxt-link to="/inventoryCards" style="text-decoration: none">
 												<span class="fs16"><img src="/images/stress.jpg" class="imgInventorySmall"/></span>
 											</nuxt-link>
 										</div>
 										<v-spacer></v-spacer>
-										<div style="position: absolute; bottom: 85px; left: 50%; width:130px; transform: translate(-50%); z-index: 2; color: white; font-size: 14px">
+										<div class="hidden-sm-and-up" style="position: absolute; bottom: 85px; left: 50%; width:130px; transform: translate(-50%); z-index: 2; color: white; font-size: 14px">
+											<span>Choisi bien ton équipement pour rapporter le plus de point de ferveur à ton équipe favorite !</span>
+										</div>
+										<div class="hidden-xs-only" style="position: absolute; bottom: 85px; left: 50%; width:150px; transform: translate(-50%); z-index: 2; color: white; font-size: 16px">
 											<span>Choisi bien ton équipement pour rapporter le plus de point de ferveur à ton équipe favorite !</span>
 										</div>
 										<v-spacer></v-spacer>
-										<div style="position: absolute; top: 5px; right: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
+										<div class="hidden-sm-and-up" style="position: absolute; top: 5px; right: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
 											<span class="inventoryBadgeAvatar fs16">Torse</span>
 										</div>
-										<div style="position: absolute; top: 80px; right: 5px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 70px; height: 70px">
+										<div class="hidden-xs-only" style="position: absolute; top: 5px; right: 115px; z-index: 2; border-radius: 5px; border: 2px solid orangered; width: 100px; height: 100px">
 											<span class="inventoryBadgeAvatar fs16">Bras</span>
 										</div>
 									</div>
