69e140be33d73ff053883634d9c82921a1ea3069
Macgama <33863329+macgama@users.noreply.github.com>
2019-04-08 15:32:47 +0200

    Add files via upload

--- pages/matchAvenir.vue
+++ pages/matchAvenir.vue
@@ -4,18 +4,47 @@
           
 			<v-layout>
 				<v-flex>
-					<v-layout>
-						<v-flex xs2 sm2 md2 left>
+					<v-layout align-center justify-center row fill-height/>
+						<v-flex xs2 sm2 md2>
 						
 						</v-flex>
-						<v-flex xs8 sm8 md8 center>
+						<v-flex xs8 sm8 md8 text-xs-center>
 							Europe, Champion's League 2018-2019<br />
 							Journée 1
 						</v-flex>
-						<v-flex xs2 sm2 md8 right>
-							<div class="text-xs-right retour">
+						<v-flex xs2 sm2 md8 text-xs-right>
+							<div class="retour">
 								<fa :icon="['fas', 'arrow-circle-left']" size="2x" class="icon" @click="goBack" />
 							</div>
+						</v-flex>
+					</v-layout>
+					<v-layout align-center justify-center row fill-height/>
+						<v-flex text-xs-left>
+							Drapeau
+						</v-flex>
+						<v-flex text-xs-left>
+							Score PF
+						</v-flex>
+						<v-flex text-xs-center>
+							Date et heure
+						</flex>
+						<v-flex text-xs-right>
+							Score PF
+						</v-flex>
+						<v-flex text-xs-right>
+							Drapeau
+						</v-flex>						
+					</v-layout>
+					<v-layout align-center justify-center row fill-height/>
+						<v-flex text-xs-left>
+							Equipe 1
+						</v-flex>
+						<v-flex text-xs-center>
+							Score réel< br/>
+							2 - 1
+						</v-flex>
+						<v-flex text-xs-right>
+							Equipe 2
 						</v-flex>
 					</v-layout>
 				</v-flex>
