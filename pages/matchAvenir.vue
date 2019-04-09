16dbe9ac085280a68d3f748c33348d8029c689bb
Macgama <33863329+macgama@users.noreply.github.com>
2019-04-09 14:35:49 +0200

    Add files via upload

--- pages/matchAvenir.vue
+++ pages/matchAvenir.vue
@@ -72,18 +72,20 @@
 					</v-flex>
 				</v-layout>
 				<v-layout grid-list-xs row wrap style="background-color: rgb(0, 0, 0, 0.9); border-top: 1px solid darkgrey; border-bottom: 1px solid darkgrey">
-					<nuxt-link to="/matchStats" class="hoverMatchStats" style="text-decoration: none; color: white">
+					<nuxt-link to="/matchStats" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">
 						<v-flex d-flex xs6 sm6 md6 justify-center align-center style="border-right: 1px solid darkgrey; ; padding: 15px; color: white; font-size: 1.2em; text-decoration: uppercase">
 							<div class="menuMatch">
 								Match 
 							</div>
 						</v-flex>
 					</nuxt-link>
-					<v-flex d-flex xs6 sm6 md6 justify-center align-center>
-						<div class="menuTif">
-							Joue avec TIF
-						</div>			
-					</v-flex>
+					<nuxt-link to="/matchStats" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">
+						<v-flex d-flex xs6 sm6 md6 justify-center align-center style="border-right: 1px solid darkgrey; ; padding: 15px; color: white; font-size: 1.2em; text-decoration: uppercase">
+							<div class="menuTif">
+								Joue avec TIF
+							</div>			
+						</v-flex>
+					</nuxt-link>
 				</v-layout>	
 				<v-layout grid-list-xs row wrap style="background-color: rgb(0, 0, 0, 0.9); padding: 15px; color: white; font-size: 1.2em; margin-top: 10px; text-decoration: uppercase; border-top: 1px solid darkgrey; border-bottom: 1px solid darkgrey">
 					<v-flex d-flex xs4 sm4 md4 justify-center align-center style="border-right: 1px solid darkgrey">
