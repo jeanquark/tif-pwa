f8ba6c39c594d31540c761219e5eafc1ced427c6
Macgama <33863329+macgama@users.noreply.github.com>
2019-04-10 11:25:11 +0200

    Add files via upload

--- pages/matchAvenir.vue
+++ pages/matchAvenir.vue
@@ -108,7 +108,7 @@
 				</v-layout>
 			</div>
 			<div class="resumeMatch">
-				<v-layout grid-list-xs row wrap style="margin-top: 20px">
+				<v-layout grid-list-xs row wrap class="tempsFortMargin">
 					<v-flex d-flex xs12 sm12 md12 justify-center align-center class="titleResume">
 						<div >
 							Temps forts du match
@@ -117,29 +117,29 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 									26'
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 									<img src="/images/soccer.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 									Nom Prénom buteur<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom passeur</span>
 								</div>			
@@ -149,31 +149,31 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 									Nom Prénom buteur<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom passeur</span>
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 									<img src="/images/soccer.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 									26'
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 								</div>			
 							</v-flex>
 						</v-layout>
@@ -181,30 +181,30 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 									Carton jaune<br />
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 									<img src="/images/jaune.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 									26'
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 								</div>			
 							</v-flex>
 						</v-layout>
@@ -212,29 +212,29 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 									26'
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 									<img src="/images/rouge.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 									Carton rouge<br />
 								</div>			
 							</v-flex>
@@ -243,31 +243,31 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 									Nom Prénom qui entre<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom qui sort</span>
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 									<img src="/images/fleche.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 									26'
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 								</div>			
 							</v-flex>
 						</v-layout>
@@ -275,29 +275,29 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 									26'
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 									<img src="/images/fleche.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 									Nom Prénom qui entre<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom qui sort</span>
 								</div>			
@@ -307,29 +307,29 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 									26'
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 									<img src="/images/soccer.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 									Nom Prénom buteur<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom passeur</span>
 								</div>			
@@ -339,31 +339,31 @@
 					<v-flex d-flex xs12 sm12 md12>
 						<v-layout grid-list-xs row wrap class="eventMatch">
 							<v-flex d-flex xs4 sm4 md4 justify-end align-center>
-								<div class="tempsFortGauche">
+								<div class="tempsFortGaucheTexte">
 									Nom Prénom buteur<br />
 									<span style="font-size: 0.9em; font-weight: 500">Nom Prénom passeur</span>
 								</div>
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center>
-								<div style="padding: 10px; text-align: right">
+								<div class="tempsfortGaucheImage">
 									<img src="/images/soccer.png" class="imgIconSmall" />
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-end align-center style="border-right: 1px dashed black">
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortGaucheTime">
 									26'
 								</div>			
 							</v-flex>
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; color: black; font-size: 1.0em; text-decoration: uppercase">
+								<div class="tempsFortDroiteTime">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs1 sm1 md1 justify-start align-center>
-								<div style="padding: 10px; text-align: left">
+								<div class="tempsFortDroiteImage">
 								</div>			
 							</v-flex>					
 							<v-flex d-flex xs4 sm4 md4 justify-start align-center>
-								<div class="tempsFortDroite">
+								<div class="tempsFortDroiteTexte">
 								</div>			
 							</v-flex>
 						</v-layout>
@@ -520,7 +520,7 @@
 	.imgIconSmall {
 	width: 25px;
 	}
-	.tempsFortGauche {
+	.tempsFortGaucheTexte {
 	padding: 10px; 
 	color: black; 
 	font-size: 1.0em; 
@@ -528,7 +528,27 @@
 	text-align: right;
 	font-weight: 700;
 	}
-	.tempsFortDroite {
+	.tempsfortGaucheImage {
+	padding: 10px; 
+	text-align: right;
+	}
+	.tempsFortGaucheTime {
+	padding: 10px; 
+	color: black; 
+	font-size: 1.0em; 
+	text-transform: uppercase;
+	}
+	.tempsFortDroiteTime {
+	padding: 10px; 
+	color: black; 
+	font-size: 1.0em; 
+	text-transform: uppercase;
+	}
+	.tempsFortDroiteImage {
+	padding: 10px; 
+	text-align: left;
+	}
+	.tempsFortDroiteTexte {
 	padding: 10px; 
 	color: black; 
 	font-size: 1.0em; 
@@ -548,6 +568,9 @@
 	color: white; 
 	font-size: 1.2em; 
 	text-transform: uppercase;
+	}
+	.tempsFortMargin {
+	margin-top: 20px
 	}
 
 	/* Small screens */
@@ -616,11 +639,44 @@
 		}
 		.menuMatch {
 		padding: 5px; 
-		font-size: 1.1em; 
+		font-size: 1.0em; 
 		}
 		.menuTif {
 		padding: 5px; 
-		font-size: 1.1em; 
+		font-size: 1.0em; 
+		}
+		.tempsFortMargin {
+		margin-top: 10px
+		}
+		.titleResume {
+		font-size: 1em;
+		}
+		.tempsFortGaucheTexte {
+		padding: 5px; 
+		font-size: 0.8em; 
+		}
+		.tempsfortGaucheImage {
+		padding: 5px; 
+		text-align: right;
+		}
+		.tempsFortGaucheTime {
+		padding: 5px; 
+		font-size: 0.9em; 
+		}
+		.tempsFortDroiteTime {
+		padding: 5px; 
+		font-size: 0.9em; 
+		}
+		.tempsFortDroiteImage {
+		padding: 5px; 
+		text-align: left;
+		}
+		.tempsFortDroiteTexte {
+		padding: 5px; 
+		font-size: 0.8em; 
+		}
+		.imgIconSmall {
+		width: 15px;
 		}
     }
 </style>
