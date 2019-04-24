afbab598701d567b75063aa0da16b718b8baad34
macgama <gael.manigley@gmail.com>
2019-04-24 21:00:21 +0200

    Maj menu joueTif

--- pages/joueTifActionCol.vue
+++ pages/joueTifActionCol.vue
@@ -84,23 +84,51 @@
 							Joue avec ton fan !
 						</div>			
 					</v-flex>
-				</v-layout>	
-				<v-layout grid-list-xs row wrap style="background-color: rgb(0, 0, 0, 0.9); border-top: 1px solid darkgrey; border-bottom: 1px solid darkgrey; margin-top: 5px; margin-bottom: 5px">
-					<v-flex d-flex xs6 sm3 md3 justify-center align-center>
+				</v-layout>
+				<v-layout grid-list-xs row wrap hidden-sm-and-up style="background-color: rgb(0, 0, 0, 0.9); border-top: 1px solid darkgrey; border-bottom: 1px solid darkgrey; margin-top: 5px; margin-bottom: 5px">
+					<v-flex d-flex sm3 md3 justify-center align-center>
 						<nuxt-link to="/joueTif" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
 							<div class="menuTif" style="border-right: 1px solid darkgrey">
 								Résumé du match 
 							</div>
 						</nuxt-link>
 					</v-flex>
-					<v-flex d-flex xs6 sm3 md3 justify-center align-center>
+					<v-flex d-flex sm3 md3 justify-center align-center>
 						<nuxt-link to="/joueTifComments" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
 							<div class="menuTif" style="border-right: 1px solid darkgrey">
 								Commentaires
 							</div>	
 						</nuxt-link>
 					</v-flex>
-					<v-flex d-flex xs6 sm3 md3 justify-center align-center>
+					<v-flex d-flex sm3 md3 justify-center align-center>
+						<nuxt-link to="/joueTifActionInd" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
+							<div class="menuTif" style="border-right: 1px solid darkgrey">
+								Actions individuelles
+							</div>	
+						</nuxt-link>
+					</v-flex>
+					<v-flex d-flex sm3 md3 justify-center align-center>
+						<div class="menuMatch">
+							Actions collectives
+						</div>
+					</v-flex>
+				</v-layout>
+				<v-layout grid-list-xs row wrap hidden-xs-only style="background-color: rgb(0, 0, 0, 0.9); border-top: 1px solid darkgrey; border-bottom: 1px solid darkgrey; margin-top: 5px; margin-bottom: 5px">
+					<v-flex d-flex xs6 justify-center align-center>
+						<nuxt-link to="/joueTif" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
+							<div class="menuTif" style="border-right: 1px solid darkgrey; border-bottom: 1px solid darkgrey">
+								Résumé du match 
+							</div>
+						</nuxt-link>
+					</v-flex>
+					<v-flex d-flex xs6 justify-center align-center>
+						<nuxt-link to="/joueTifComments" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
+							<div class="menuTif" style="border-right: 1px solid darkgrey; border-bottom: 1px solid darkgrey">
+								Commentaires
+							</div>	
+						</nuxt-link>
+					</v-flex>
+					<v-flex d-flex xs6 justify-center align-center>
 						<nuxt-link to="/joueTifActionInd" class="hoverMatchStats" style="text-decoration: none; color: white; width: 100%">	
 							<div class="menuTif" style="border-right: 1px solid darkgrey">
 								Actions individuelles
