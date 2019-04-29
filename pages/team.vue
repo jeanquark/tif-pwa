e608aa89c9791c496513deb77bc7ec8f93c81b29
Macgama <33863329+macgama@users.noreply.github.com>
2019-04-29 17:02:10 +0200

    Add files via upload

--- pages/team.vue
+++ pages/team.vue
@@ -153,9 +153,120 @@
 					</v-flex>
 				</v-layout>
 			</div>
-			
-			
+			<div class="resumeMatch">
+				<v-layout grid-list-xs row wrap style="margin-top: 10px">
+					<v-flex d-flex xs12 sm12 md12 justify-center align-center class="titleResume">
+						<div>
+							Tous les matchs de la saison
+						</div>
+					</v-flex>
+					<v-flex d-flex xs12 sm12 md12>
+						<v-card flat>
+							<v-card-text style="padding: 8px">							
+								<div slot="header" class="white--text">
+									<div v-for="country in competition.countries" :key="country.slug" style="display: flex; align-items:center;">
+										<img :src="'/images/countries/' + country.slug + '.png'" style="width: 18px; height: 18px" />&nbsp;&nbsp;{{ competition.name }}
+									</div>
+								</div>
+								<v-icon slot="actions" color="white">$vuetify.icons.expand</v-icon>
+								<v-card>
+									<v-card-text style="padding: 0">
+										<v-data-table :items="loadedEventsByDay(day)['events'].filter(event => event.league_slug === competition.slug)" no-data-text="No game found on this day." class="elevation-0" hide-actions hide-headers v-if="loadedEventsByDay(day)">
+											<template slot="items" slot-scope="props" style="height: 15px; border-spacing: 0; padding: 2px; border: 1px solid black">
+												<v-layout align-center style="padding: 0; border-right: 1px solid black; border-left: 1px solid black; border-bottom: 1px solid black">
+													<v-flex xs12 style="margin: 0; padding-top: 2px; padding-bottom: 2px; height: 100%">
+														<v-layout align-start>
+															<v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 15px; margin: 0">
+																<div style="background-color: green; height: 100%; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
+																<div style="background-color: orangered; height: 100%; width: 2px" v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'"></div>
+															</v-flex>
+															<v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
+																<div style="color: orange;font-size: 80%">
+																	<span style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;" v-if="props.item.statusShort === 'FT' || props.item.statusShort === 'AET'">Finished</span>
+																	<span style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;" v-if="props.item.statusShort === '1H' || props.item.statusShort === '2H'">{{ props.item.elapsed }} min</span>
+																	<span v-if="props.item.statusShort === 'HT'" style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;">Half time</span>
+																	<span v-if="props.item.statusShort === 'NS'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >{{ convertToLocalTime(props.item.timestamp) }}</span>
+																	<span v-if="props.item.statusShort === 'PST'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match postponed</span>
+																	<span v-if="props.item.statusShort === 'Abd'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match abandonned</span>
+																	ID: {{ props.item.id }}
+																</div>
+															</v-flex>
 
+														</v-layout>
+
+														<v-layout align-center style="max-width: 100%">
+															<v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 40px; margin: 0">
+																<div style="background-color: green; height: 40px; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
+																<div v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'" style="background-color: orangered; height: 40px; width: 2px"></div>
+															</v-flex>
+															<v-flex sm1 hidden-xs-only align-center class="text-xs-center imgTeamLogoWrapper" style="">
+																<nuxt-link to="/team" style="text-decoration: none; color: black">
+																	<img :src="'/images/teams/' + props.item.homeTeam_slug + '.png'" :lazy-src="'/images/icon.png'" class="imgTeamLogo"/>
+																</nuxt-link>
+															</v-flex>
+															<v-flex sm4 xs5 align-center class="text-xs-left pd-left10">
+																<nuxt-link to="/team" style="text-decoration: none; color: black">
+																	<span class="teamTextSize"><v-icon class="notYourTeam">star</v-icon> {{ props.item.homeTeam_name }}</span>
+																</nuxt-link>
+															</v-flex>
+															<v-flex sm2 xs2 class="text-xs-center">
+																<nuxt-link to="/matchTermine">
+																	<span class="scoreBox" style="" v-if="props.item.statusShort === 'FT' || props.item.statusShort === 'AET'">
+																		{{ props.item.final_score }}
+																	</span>
+																</nuxt-link>
+																<nuxt-link to="/matchEncours">
+																	<span class="scoreBox" style="" v-if="props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'">
+																		<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
+																			<span :key="props.item.goalsHomeTeam">
+																				{{ props.item.goalsHomeTeam }}
+																			</span>
+																		</transition> -
+																		<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
+																			<span :key="props.item.goalsVisitorTeam">
+																				{{ props.item.goalsVisitorTeam }}
+																			</span>
+																		</transition>
+																	</span>
+																</nuxt-link>
+																<nuxt-link to="/matchAvenir">
+																	<span class="scoreBox" style="" v-if="props.item.statusShort === 'NS'">
+																		{{ convertToLocalTime(props.item.timestamp) }}
+																	</span>
+																</nuxt-link>
+																<nuxt-link to="/matchAvenir">
+																	<span class="scoreBox" style="" v-if="props.item.statusShort === 'PST'">
+																		Postponed
+																	</span>
+																</nuxt-link>
+																<nuxt-link to="/matchAvenir">
+																	<span class="scoreBox" style="" v-if="props.item.statusShort === 'Abd'">
+																		Abandonned
+																	</span>
+																</nuxt-link
+															</v-flex>
+															<v-flex sm4 xs5 align-center class="text-xs-right pd-right10">
+																<nuxt-link to="/team" style="text-decoration: none; color: black">
+																	<span class="teamTextSize">{{ props.item.visitorTeam_name }} <v-icon class="notYourTeam">star</v-icon></span>
+																</nuxt-link>
+															</v-flex>
+															<v-flex sm1 hidden-xs-only align-center class="text-xs-center imgTeamLogoWrapper" style="">
+																<nuxt-link to="/team" style="text-decoration: none; color: black">
+																	<img :src="'/images/teams/' + props.item.visitorTeam_slug + '.png'" :lazy-src="'/images/icon.png'" class="imgTeamLogo"/>
+																</nuxt-link>
+															</v-flex>
+														</v-layout>
+													</v-flex>
+												</v-layout>
+											</template>
+										</v-data-table>
+									</v-card-text>
+								</v-card>
+							</v-card-text>
+						</v-card>
+					</v-flex>
+				</v-layout>
+			</div>
 			<div class="resumeMatch">
 				<v-layout grid-list-xs row wrap style="margin-top: 10px">
 					<v-flex d-flex xs12 sm12 md12 justify-center align-center class="titleResume">
@@ -177,14 +288,26 @@
 
 <script>
     import ActionsCard from '~/components/ActionsCard'
-    export default {
-		components: { 
-			Login: () => import('~/components/Login'), 
-			Register: () => import('~/components/Register')
-		},
+    export default default {
+		async created () {
+            this.$store.commit('setLoading', true)
+			for (let i = 15; i >= 1; i--) {
+                this.days.push(moment().subtract(i, 'days').format('YYYY-MM-DD'))
+            }
+            this.days.push(moment().format('YYYY-MM-DD'))
+            for (let i = 1; i <= 15; i++) {
+                this.days.push(moment().add(i, 'days').format('YYYY-MM-DD'))
+            }
+
+            const today = moment().format('YYYY-MM-DD')
+            await this.$store.dispatch('events/fetchEventsByDay', today)
+            this.$store.commit('setLoading', false)
+        },
         layout: 'layoutFront',
 		data () {
 			return {
+				activeDay: `${moment().format('YYYY-MM-DD')}`,
+				days: [],
 				loginModal: false,
 				registerModal: false,
 				bottomNav: 'la_liga_18_19'
@@ -193,9 +316,27 @@
 		computed: {
 			loadedUser () {
 				return this.$store.getters['users/loadedUser']
-			}
+			},
+			loadedEvents () {
+                return this.$store.getters['events/loadedEvents']
+            },
+            loadedActiveCompetitions () {
+                return this.$store.getters['competitions/loadedCompetitions'].filter(competition => competition.status === 'active')
+            }
 		},
         methods: {
+			async fetchEventsByDay(date) {
+                this.$store.commit('setLoading', true)
+                console.log('fetchEventsByDay: ', date)
+                if (!this.loadedEvents[date]) {
+                    await this.$store.dispatch('events/fetchEventsByDay', date)
+                }
+                // const that = this
+                // setTimeout(() => {
+                //     that.$store.commit('setLoading', false)
+                // }, 1000)
+                this.$store.commit('setLoading', false)
+            },
 			async logout () {
 				await this.$store.dispatch('firebase-auth/signOut')
 			},
@@ -215,7 +356,13 @@
 			},
 			goBack() {
 				this.$router.replace("/gamemode_gm")
-			}
+			},
+            loadedEventsByDay (date) {
+                return this.$store.getters['events/loadedEvents'][date]
+            },
+            convertToLocalTime (timestamp) {
+                return moment.unix(timestamp).format('HH:mm')
+            }
         }
     }
 </script>
