2968ef332b38a0e058682483a6cdad33bd5d546d
Macgama <33863329+macgama@users.noreply.github.com>
2019-05-14 10:46:35 +0200

    Add files via upload

--- serverMiddleware/registerNewUser.js
+++ serverMiddleware/registerNewUser.js
@@ -18,7 +18,7 @@ function buildUserObject (payload) {
     user.id = payload.id,
     user.email = payload.email,
     user.pseudo = payload.pseudo ? payload.pseudo : '',
-	// user.birthyear = payload.userbirthyear ? payload.userbirthyear : '',
+	user.birthyear = payload.userbirthyear ? payload.userbirthyear : '',
     user.country = {
         name: payload.country.name ? payload.country.name : '',
         slug: payload.country.slug ? payload.country.slug : ''
