b6af43842952401a2da98e0022d3671f6c91a8ff
Macgama <33863329+macgama@users.noreply.github.com>
2019-05-16 15:28:54 +0200

    Add files via upload

--- components/ScoremodeResults.vue
+++ components/ScoremodeResults.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<v-tabs color="rgb(0,0,0)" height="30" justify-center fixed-tabs show-arrows v-model="activeDay" @change="fetchEventsByDay(activeDay)">
+	<v-tabs color="rgb(0,0,0)" height="35" justify-center fixed-tabs show-arrows v-model="activeDay" @change="fetchEventsByDay(activeDay)">
 	<v-tabs-slider color="white"></v-tabs-slider>
 		<v-tab v-for="day in days" :key="day" :href="'#' + day">
 			<span style="font-size: 1.0em; color: white">{{ day | moment('ddd DD MMM') }}</span>
