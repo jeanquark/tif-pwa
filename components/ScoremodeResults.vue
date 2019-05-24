731f0bfeab21b28a9577e235ad72a5951308ecb1
Macgama <33863329+macgama@users.noreply.github.com>
2019-05-24 14:32:52 +0200

    Add files via upload

--- components/ScoremodeResults.vue
+++ components/ScoremodeResults.vue
@@ -35,16 +35,17 @@
                                                                             <v-layout align-start>
                                                                                 <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 15px; margin: 0">
                                                                                     <div style="background-color: green; height: 100%; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
-                                                                                    <div style="background-color: orangered; height: 100%; width: 2px" v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'"></div>
+                                                                                    <div style="background-color: orangered; height: 100%; width: 2px" v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'ABD' || props.item.statusShort === 'AET'"></div>
                                                                                 </v-flex>
                                                                                 <v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
                                                                                     <div style="color: orange;font-size: 80%">
                                                                                         <span style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;" v-if="props.item.statusShort === 'FT' || props.item.statusShort === 'AET'">Finished</span>
                                                                                         <span style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;" v-if="props.item.statusShort === '1H' || props.item.statusShort === '2H'">{{ props.item.elapsed }} min</span>
                                                                                         <span v-if="props.item.statusShort === 'HT'" style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;">Half time</span>
-                                                                                        <span v-if="props.item.statusShort === 'NS'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >{{ convertToLocalTime(props.item.timestamp) }}</span>
-                                                                                        <span v-if="props.item.statusShort === 'PST'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match postponed</span>
-                                                                                        <span v-if="props.item.statusShort === 'Abd'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match abandonned</span>
+                                                                                        <span v-if="props.item.statusShort === 'NS'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px">{{ convertToLocalTime(props.item.timestamp) }}</span>
+                                                                                        <span v-if="props.item.statusShort === 'TBD'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px">Time to be defined</span>
+																						<span v-if="props.item.statusShort === 'PST'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px">Match postponed</span>
+                                                                                        <span v-if="props.item.statusShort === 'ABD'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px">Match abandonned</span>
                                                                                         ID: {{ props.item.id }}
                                                                                     </div>
                                                                                 </v-flex>
@@ -54,7 +55,7 @@
                                                                             <v-layout align-center style="max-width: 100%">
                                                                                 <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 40px; margin: 0">
                                                                                     <div style="background-color: green; height: 40px; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
-                                                                                    <div v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'" style="background-color: orangered; height: 40px; width: 2px"></div>
+                                                                                    <div v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'ABD' || props.item.statusShort === 'TBD' || props.item.statusShort === 'AET'" style="background-color: orangered; height: 40px; width: 2px"></div>
                                                                                 </v-flex>
                                                                                 <v-flex sm1 hidden-xs-only align-center class="text-xs-center imgTeamLogoWrapper" style="">
 																					<nuxt-link to="/team">
@@ -88,7 +89,10 @@
                                                                                     <span class="scoreBox" style="" v-if="props.item.statusShort === 'PST'">
                                                                                         Postponed
                                                                                     </span>
-                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'Abd'">
+                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'TBD'">
+                                                                                        Time To Be Definied
+                                                                                    </span>
+                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'ABD'">
                                                                                         Abandonned
                                                                                     </span>
                                                                                 </v-flex>
@@ -137,7 +141,7 @@
                                                                             <v-layout align-start>
                                                                                 <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 15px; margin: 0">
                                                                                     <div style="background-color: green; height: 100%; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
-                                                                                    <div style="background-color: orangered; height: 100%; width: 2px" v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'"></div>
+                                                                                    <div style="background-color: orangered; height: 100%; width: 2px" v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'ABD' || props.item.statusShort === 'AET'"></div>
                                                                                 </v-flex>
                                                                                 <v-flex class="text-xs-left" style="width: 100%; padding: 0; height: 15px; margin: 0">
                                                                                     <div style="color: orange;font-size: 80%">
@@ -145,8 +149,9 @@
                                                                                         <span style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;" v-if="props.item.statusShort === '1H' || props.item.statusShort === '2H'">{{ props.item.elapsed }} min</span>
                                                                                         <span v-if="props.item.statusShort === 'HT'" style="float: left; background-color: green; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px;">Half time</span>
                                                                                         <span v-if="props.item.statusShort === 'NS'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >{{ convertToLocalTime(props.item.timestamp) }}</span>
-                                                                                        <span v-if="props.item.statusShort === 'PST'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match postponed</span>
-                                                                                        <span v-if="props.item.statusShort === 'Abd'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match abandonned</span>
+                                                                                        <span v-if="props.item.statusShort === 'TBD'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Time to be Definied</span>
+																						<span v-if="props.item.statusShort === 'PST'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match postponed</span>
+                                                                                        <span v-if="props.item.statusShort === 'ABD'" style="float: left; background-color: orangered; color: white; text-align: center; padding-left: 5px; padding-right: 5px; margin-right: 5px" >Match abandonned</span>
                                                                                         ID: {{ props.item.id }}
                                                                                     </div>
                                                                                 </v-flex>
@@ -156,7 +161,7 @@
                                                                             <v-layout align-center style="max-width: 100%">
                                                                                 <v-flex class="text-xs-left" style="width: 4px; padding-left: 2px; padding-right: 2px; height: 40px; margin: 0">
                                                                                     <div style="background-color: green; height: 40px; width: 2px" v-if="props.item.statusShort === 'FT' || props.item.statusShort === '1H' || props.item.statusShort === '2H' || props.item.statusShort === 'HT'"></div>
-                                                                                    <div v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'Abd' || props.item.statusShort === 'AET'" style="background-color: orangered; height: 40px; width: 2px"></div>
+                                                                                    <div v-if="props.item.statusShort === 'NS' || props.item.statusShort === 'PST' || props.item.statusShort === 'ABD' || props.item.statusShort === 'TBD' || props.item.statusShort === 'AET'" style="background-color: orangered; height: 40px; width: 2px"></div>
                                                                                 </v-flex>
                                                                                 <v-flex sm1 hidden-xs-only align-center class="text-xs-center imgTeamLogoWrapper" style="">
 																					<nuxt-link to="/team">
@@ -190,7 +195,10 @@
                                                                                     <span class="scoreBox" style="" v-if="props.item.statusShort === 'PST'">
                                                                                         Postponed
                                                                                     </span>
-                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'Abd'">
+                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'TBD'">
+                                                                                        Time To Be Definied
+                                                                                    </span>
+                                                                                    <span class="scoreBox" style="" v-if="props.item.statusShort === 'ABD'">
                                                                                         Abandonned
                                                                                     </span>
                                                                                 </v-flex>
